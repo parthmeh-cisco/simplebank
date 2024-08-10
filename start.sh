eb31a74b8e94637f9f85c2b79024fb5fe64707b8
Partha Mehta <parthmeh@cisco.com>
2024-08-10 19:49:22 -0400

    more debug. (#16)

--- start.sh
+++ start.sh
@@ -3,15 +3,17 @@
 set -e
 
 echo "Sourcing environment variables..."
-if [ -f /app/app.env ]; then
-    source /app/app.env
+if [ -f app.env ]; then
+    echo "app.env found. Contents:"
+    cat app.env
+    source app.env
     echo "DB_SOURCE after sourcing: $DB_SOURCE"
 else
     echo "app.env file not found!"
 fi
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+migrate -path db/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
