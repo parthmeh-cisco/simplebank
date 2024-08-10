e1b604a810efe5786cfc4cc7d12a34510667de4f
Partha Mehta <parthmeh@cisco.com>
2024-08-10 02:58:56 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-source /app/.env
+source /app/app.env
 # Debug output
 echo "DB_SOURCE is set to: $DB_SOURCE"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
