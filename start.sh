dc7150f7924b13c4b832e9fff3ade1dc23a5c67d
Partha Mehta <parthmeh@cisco.com>
2024-08-28 19:10:18 -0400

    load environment variable before running db migration. (#20)

--- start.sh
+++ start.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-echo "Sourcing environment variables..."
-if [ -f /app/app.env ]; then
-    source /app/app.env
-    echo "DB_SOURCE after sourcing: $DB_SOURCE"
-else
-    echo "app.env file not found!"
-fi
+# echo "Sourcing environment variables..."
+# if [ -f /app/app.env ]; then
+#     source /app/app.env
+#     echo "DB_SOURCE after sourcing: $DB_SOURCE"
+# else
+#     echo "app.env file not found!"
+# fi
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
