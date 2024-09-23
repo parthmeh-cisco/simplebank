0ab62149de2bf13b8897c778621d87c0037766be
Partha Mehta <parthmeh@cisco.com>
2024-09-23 17:26:34 -0400

    use kubectl apply (#37)

---
+++ eks/install.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.2/deploy/static/provider/aws/deploy.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
